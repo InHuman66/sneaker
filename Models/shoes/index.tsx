d23fd36c148c9691f17b30b4538b35a7b203b275
inHuman66 <olexandr.kk@gmail.com>
2022-01-20 10:25:14 +0200

    add new custom model(try to setup texture)

--- Models/shoes/index.tsx
+++ Models/shoes/index.tsx
@@ -1,47 +1,11 @@
+
 import * as THREE from 'three'
 import React, {useRef, useState} from 'react'
 import { useGLTF } from '@react-three/drei'
 import { GLTF } from 'three-stdlib'
-import {proxy} from "valtio";
-import {useSnapshot} from "valtio";
+import { proxy,useSnapshot } from 'valtio'
 
 
-type GLTFResult = GLTF & {
-    nodes: {
-        shoe: THREE.Mesh
-        shoe_1: THREE.Mesh
-        shoe_2: THREE.Mesh
-        shoe_3: THREE.Mesh
-        shoe_4: THREE.Mesh
-        shoe_5: THREE.Mesh
-        shoe_6: THREE.Mesh
-        shoe_7: THREE.Mesh
-    }
-    materials: {
-        laces: THREE.MeshStandardMaterial
-        mesh: THREE.MeshStandardMaterial
-        caps: THREE.MeshStandardMaterial
-        inner: THREE.MeshStandardMaterial
-        sole: THREE.MeshStandardMaterial
-        stripes: THREE.MeshStandardMaterial
-        band: THREE.MeshStandardMaterial
-        patch: THREE.MeshStandardMaterial
-    }
-}
-let state: StateType =proxy({
-    current:null,
-    items:{
-        laces: "#ff0000",
-        mesh: "#ffffff",
-        caps: "#ffffff",
-        inner: "#ffffff",
-        sole: "#000000",
-        stripes: "#ffffff",
-        band: "#ffffff",
-        patch: "#ffffff"
-    }
-
-})
 type StateType = {
     current: null | string,
     items:{
@@ -56,32 +20,90 @@ type StateType = {
     }
 }
 
+
+type GLTFResult = GLTF & {
+    nodes: {
+        shoeshoe_leftpolySurface60polySurface49polySurface59: THREE.Mesh
+        shoeshoe_leftpolySurface49polySurface59polySurface57: THREE.Mesh
+        shoeshoe_leftpolySurface49polySurface59polySurface59: THREE.Mesh
+        shoepolySurface65: THREE.Mesh
+        shoegroup1polySurface22polySurface35shoe_left: THREE.Mesh
+        shoeshoe_leftpolySurface49polySurface59polySurface55: THREE.Mesh
+        shoeshoe_leftpolySurface49polySurface59polySurface56: THREE.Mesh
+        shoeshoe_leftpolySurface7group1polySurface32: THREE.Mesh
+        shoeshoe_leftpolySurface49polySurface59polySurface61: THREE.Mesh
+        shoegroup1polySurface12polySurface34: THREE.Mesh
+        shoeshoe_leftgroup1polySurface24polySurface36: THREE.Mesh
+        shoeshoe_leftpolySurface7group1polySurface31: THREE.Mesh
+        shoeshoe_leftgroup1polySurface13polySurface34: THREE.Mesh
+    }
+    materials: {
+        initialShadingGroup: THREE.MeshStandardMaterial
+    }
+}
+
 export default function Shoes({ ...props }: JSX.IntrinsicElements['group']) {
     const group = useRef<THREE.Group>()
-    const snap  = useSnapshot(state)
-    let [hovered, setHovered] = useState<null | string>(null)
-    const { nodes, materials } = useGLTF('/shoe-draco.glb') as GLTFResult
+    const { nodes, materials } = useGLTF('/shoe05.glb') as GLTFResult
     return (
-        <group
-            ref={group}
-            {...props}
-            dispose={null}
-            onPointerOver={(e)=>{e.stopPropagation, setHovered(e.object.name)}}
-            onPointerOut={(e)=>{e.intersections.length === 0 && setHovered(null) }}
-            onPointerDown={(e)=>{e.stopPropagation(); state.current = e.object.name }}
-            onPointerMissed={(e)=>{state.current = null}}
-        >
-            <mesh name={'laces'} material-color={snap.items.laces}  geometry={nodes.shoe.geometry} material={materials.laces} />
-            <mesh material-color={snap.items.mesh}  geometry={nodes.shoe_1.geometry} material={materials.mesh} />
-            <mesh material-color={snap.items.caps} geometry={nodes.shoe_2.geometry} material={materials.caps} />
-            <mesh material-color={snap.items.inner}  geometry={nodes.shoe_3.geometry} material={materials.inner} />
-            <mesh material-color={snap.items.sole}  geometry={nodes.shoe_4.geometry} material={materials.sole} />
-            <mesh material-color={snap.items.stripes}  geometry={nodes.shoe_5.geometry} material={materials.stripes} />
-            <mesh material-color={snap.items.band}  geometry={nodes.shoe_6.geometry} material={materials.band} />
-            <mesh material-color={snap.items.patch}  geometry={nodes.shoe_7.geometry} material={materials.patch} />
+        <group ref={group} {...props} dispose={null}>
+            <group rotation={[0.2,0.9,0]} position={[0, 0, 0]} scale={2}>
+                <mesh
+                    geometry={nodes.shoeshoe_leftpolySurface60polySurface49polySurface59.geometry}
+                    material={nodes.shoeshoe_leftpolySurface60polySurface49polySurface59.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftpolySurface49polySurface59polySurface57.geometry}
+                     material={nodes.shoeshoe_leftpolySurface49polySurface59polySurface57.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftpolySurface49polySurface59polySurface59.geometry}
+                    material={nodes.shoeshoe_leftpolySurface49polySurface59polySurface59.material}
+                />
+                <mesh geometry={nodes.shoepolySurface65.geometry}
+                      material={nodes.shoepolySurface65.material}
+                />
+                <mesh
+                    geometry={nodes.shoegroup1polySurface22polySurface35shoe_left.geometry}
+                    material={nodes.shoegroup1polySurface22polySurface35shoe_left.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftpolySurface49polySurface59polySurface55.geometry}
+                    material={nodes.shoeshoe_leftpolySurface49polySurface59polySurface55.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftpolySurface49polySurface59polySurface56.geometry}
+                    material={nodes.shoeshoe_leftpolySurface49polySurface59polySurface56.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftpolySurface7group1polySurface32.geometry}
+                    material={nodes.shoeshoe_leftpolySurface7group1polySurface32.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftpolySurface49polySurface59polySurface61.geometry}
+                    material={nodes.shoeshoe_leftpolySurface49polySurface59polySurface61.material}
+                />
+                <mesh
+                    geometry={nodes.shoegroup1polySurface12polySurface34.geometry}
+                    material={nodes.shoegroup1polySurface12polySurface34.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftgroup1polySurface24polySurface36.geometry}
+                    material={nodes.shoeshoe_leftgroup1polySurface24polySurface36.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftpolySurface7group1polySurface31.geometry}
+                    material={nodes.shoeshoe_leftpolySurface7group1polySurface31.material}
+                />
+                <mesh
+                    geometry={nodes.shoeshoe_leftgroup1polySurface13polySurface34.geometry}
+                    material={nodes.shoeshoe_leftgroup1polySurface13polySurface34.material}
+                />
+            </group>
         </group>
     )
 }
 
 
-useGLTF.preload('/shoe-draco.glb')
+
+useGLTF.preload('/shoe05.glb')
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,112 @@
 import type { NextPage } from 'next'
 import classes from '../styles/Home.module.css'
 import {Canvas} from "@react-three/fiber";
-import { Suspense } from 'react';
+import React, {Suspense} from 'react';
+import {OrbitControls} from '@react-three/drei';
 import Shoes from "../Models/shoes";
-import { Camera } from 'three/src/cameras/Camera';
-import { OrbitControls } from '@react-three/drei';
+
+
+// type GLTFResult = GLTF & {
+//     nodes: {
+//         shoe: THREE.Mesh
+//         shoe_1: THREE.Mesh
+//         shoe_2: THREE.Mesh
+//         shoe_3: THREE.Mesh
+//         shoe_4: THREE.Mesh
+//         shoe_5: THREE.Mesh
+//         shoe_6: THREE.Mesh
+//         shoe_7: THREE.Mesh
+//     }
+//     materials: {
+//         laces: THREE.MeshStandardMaterial
+//         mesh: THREE.MeshStandardMaterial
+//         caps: THREE.MeshStandardMaterial
+//         inner: THREE.MeshStandardMaterial
+//         sole: THREE.MeshStandardMaterial
+//         stripes: THREE.MeshStandardMaterial
+//         band: THREE.MeshStandardMaterial
+//         patch: THREE.MeshStandardMaterial
+//     }
+// }
+// let state: StateType =proxy({
+//     current:null,
+//     items:{
+//         laces: "#ff0000",
+//         mesh: "#ffffff",
+//         caps: "#ffffff",
+//         inner: "#ffffff",
+//         sole: "#ffffff",
+//         stripes: "#ffffff",
+//         band: "#ffffff",
+//         patch: "#ffffff"
+//     }
+//
+// })
+// type StateType = {
+//     current: null | string,
+//     items:{
+//         laces: string,
+//         mesh: string,
+//         caps: string,
+//         inner: string,
+//         sole: string,
+//         stripes: string,
+//         band: string,
+//         patch: string
+//     }
+// }
+// function Shoes({ ...props }: JSX.IntrinsicElements['group']) {
+//     const group = useRef<THREE.Group>()
+//     const snap  = useSnapshot(state)
+//     let [hovered, setHovered] = useState<null | string>(null)
+//     const { nodes, materials } = useGLTF('/shoe-draco.glb') as GLTFResult
+//     return (
+//         <group
+//             ref={group}
+//             {...props}
+//             dispose={null}
+//             onPointerOver={(e)=>{e.stopPropagation, console.log((e.object as any).material.name)}}
+//             onPointerOut={(e)=>{e.intersections.length === 0 && setHovered(null) }}
+//             onPointerDown={(e)=>{e.stopPropagation(); state.current = (e.object as any).material.name}}
+//             onPointerMissed={(e)=>{state.current = null}}
+//         >
+//             <mesh material-color={snap.items.laces}  geometry={nodes.shoe.geometry} material={materials.laces} />
+//             <mesh material-color={snap.items.mesh}  geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+//             <mesh material-color={snap.items.caps} geometry={nodes.shoe_2.geometry} material={materials.caps} />
+//             <mesh material-color={snap.items.inner}  geometry={nodes.shoe_3.geometry} material={materials.inner} />
+//             <mesh material-color={snap.items.sole}  geometry={nodes.shoe_4.geometry} material={materials.sole} />
+//             <mesh material-color={snap.items.stripes}  geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+//             <mesh material-color={snap.items.band}  geometry={nodes.shoe_6.geometry} material={materials.band} />
+//             <mesh material-color={snap.items.patch}  geometry={nodes.shoe_7.geometry} material={materials.patch} />
+//         </group>
+//     )
+// }
+// useGLTF.preload('/shoe-draco.glb')
+//
+// function  Picker(){
+//     const snap = useSnapshot(state)
+//
+//     return(
+//         <div className={classes.color_pick}>
+//             <HexColorPicker
+//                 // @ts-ignore
+//                 color={snap.items[snap.current]}
+//                 // @ts-ignore
+//                 onChange={(color)=>{state.items[snap.current]= color}}
+//             />
+//             <h1>{snap.current}</h1>
+//         </div>
+//     )
+// }
 
 const Home: NextPage = () => {
+
   return (
     <div className={classes.page_wrapper}>
       <Canvas>
-
         <ambientLight intensity={0.5}/>
         <Suspense fallback={null}>
-          <Shoes />
+          <Shoes/>
         </Suspense>
           <OrbitControls addEventListener={undefined} hasEventListener={undefined} removeEventListener={undefined} dispatchEvent={undefined} />
       </Canvas>
